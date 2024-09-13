90680bbbd9065c83de7185e2640908a69962a656
Erica Dsouza <erica.dsouza@sportzinteractive.com>
2024-09-13 17:08:21 +0530

    Day 1

---
+++ Operators.js
@@ -0,0 +1,27 @@
+var num1 =7;
+var num2 = 5;
+
+console.log (num1 + num2);
+
+var answer = num1<num2;
+
+console.log("Is num1 less than num2 : ",(answer));
+
+
+console.log (num1 - num2);
+console.log (num1 * num2);
+console.log (num1 / num2);
+console.log (num1 % num2);
+
+
+//--------------------------------------------------------------------------//
+
+var sellingPrice = 190;
+var listPrice = 270;
+var discount = (listPrice - sellingPrice) / listPrice * 100;
+console.log("Discount : "+ discount);
+
+displaydiscountPercentage = Math.round(discount);
+
+console.log("Discount : "+ displaydiscountPercentage + "%");
+   
