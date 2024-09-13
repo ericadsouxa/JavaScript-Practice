ee61155b5d1ff85ad03d4673cb249d00318d0ec0
Erica Dsouza <erica.dsouza@sportzinteractive.com>
2024-09-13 18:01:20 +0530

    Day 1

---
+++ Conditional.js
@@ -0,0 +1,8 @@
+var temeperature = 25; // 0 - 50
+
+if (temeperature < 50) { // 25 < 50
+    console.log("It's too hot outside"); 
+}
+else{
+    console.log("The weather is good today");
+}
---
+++ ConditionalLogin.js
@@ -0,0 +1,13 @@
+//Allow user to access course if he is:
+
+var email = 'QbJkQ@example.com';
+var password = '123456';
+
+
+
+if (email == 'QbJkQ@example.com' && password == '123456') {
+    console.log('Access granted');
+}
+else {
+    console.log('Access denied');
+}
